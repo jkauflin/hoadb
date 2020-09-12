0af7f2fb6b464c2733ee2cc70a2d7ebb37e89d46
John J Kauflin <johnkauflin@gmail.com>
2020-09-12 09:22:00 -0400

    Test email3

--- handlePaymentNotificationTEST.php
+++ handlePaymentNotificationTEST.php
@@ -3,7 +3,12 @@ define("LOG_FILE", "./paypal-ipn-TEST.log");
 
 echo 'LOG_FILE = ' . LOG_FILE . '</br>' . PHP_EOL;
 
-error_log(date('[Y-m-d H:i] '). "Invalid IPN: $req" . PHP_EOL, 3, LOG_FILE);
+error_log(date('[Y-m-d H:i] '). "Before mail" . PHP_EOL, 3, LOG_FILE);
+
+echo mail("johnkauflin@gmail.com","TEST paypal resend","testing the email");
+
+error_log(date('[Y-m-d H:i] ') . '>>> AFTER email send TEST ' . PHP_EOL, 3, LOG_FILE);
+
 
 echo 'At the end ' . '</br>' . PHP_EOL;
 
